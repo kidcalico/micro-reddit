6687cec65f6f2d8fb3cba6a3c7d1893974278f58
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-05-12 17:46:00 -0700

    Add custom error message for username length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
+  validates :username, presence: true, length: { in: 4..12, message: "Username must be between 4-12 characters in length." },
+              uniqueness: true
   has_many :posts
   has_many :comments
 end
